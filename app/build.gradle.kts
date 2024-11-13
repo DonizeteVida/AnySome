b2be9e274dc8e6fd4cec0c2668fe5128c1d5ebce
DonizeteVida <donizetevida1998@outlook.com>
2024-11-13 19:25:09 -0300

    Updating dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.any.some"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.any.some"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
